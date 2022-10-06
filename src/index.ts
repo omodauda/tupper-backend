6f88d109a9d466e7bf390ec4a030c3cae3bff473
omodauda <omodauda.dl@gmail.com>
2022-10-06 10:31:05 +0100

    App health check route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import dotenv from 'dotenv';
+import HealthRoute from './routes/health.route'
+import UserRoute from './routes/user.route';
 import App from './app';
 
 dotenv.config();
 
-const app = new App([]);
+const app = new App([
+  new HealthRoute(),
+  new UserRoute(),
+]);
 
 app.listen();
