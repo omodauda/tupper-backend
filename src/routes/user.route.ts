f84f4f2081903ad17c1a66ecc51eaa931fd61177
omodauda <omodauda.dl@gmail.com>
2022-10-06 10:30:29 +0100

    User signup route

---
+++ src/routes/user.route.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import UserController from "../controllers/user.controller";
+import Route from "../interfaces/route.interface";
+import validationMiddleware from "../middlewares/validation.middleware";
+import { signUpValidation } from "../validations/user.validation";
+
+export default class UserRoute implements Route {
+  public path = '/user';
+  public router = Router();
+  private UserController = new UserController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router
+      .route(`${this.path}/signup`)
+      .post(validationMiddleware(signUpValidation), this.UserController.signUp);
+  }
+}
