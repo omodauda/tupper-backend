72693b8f9dee215735e27c5b8397552c1e54990e
omodauda <omodauda.dl@gmail.com>
2022-10-06 09:07:16 +0100

    Create express application

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import hpp from 'hpp';
+
+import errorMiddleware from './middlewares/error.middleware';
+import Routes from './interfaces/route.interface';
+
+class App {
+  public app: Application;
+  public port: number | string;
+  public env: string;
+
+  constructor(routes: Routes[]) {
+    this.app = express();
+    this.port = process.env.PORT || 4000;
+    this.env = process.env.NODE_ENV || 'development';
+
+    this.initializeMiddleware();
+    this.initializeRoutes(routes);
+    this.initializeErrorHandling();
+  }
+
+  private initializeMiddleware(): void {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(morgan('dev'));
+    this.app.use(helmet());
+    this.app.use(hpp());
+  }
+
+  private initializeRoutes(routes: Routes[]): void {
+    routes.forEach(route => {
+      this.app.use('/api', route.router);
+    })
+  }
+
+  private initializeErrorHandling(): void {
+    this.app.use(errorMiddleware);
+  }
+
+  public listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`App is listening on port ${this.port}`)
+    })
+  }
+}
+
+export default App;
