a834ab2d0ba4b4ed439ee93485efbc54bf4d9f77
omodauda <omodauda.dl@gmail.com>
2022-10-08 18:22:09 +0100

    Update generate otp util

--- src/utils/otp.ts
+++ src/utils/otp.ts
@@ -2,7 +2,8 @@ import moment from 'moment';
 
 function generateOtp() {
   const otp = '1234';
-  const expiresAt = moment().add(6, 'minutes').toString();
+  // const expiresAt = moment().add(6, 'minutes').toString();
+  const expiresAt = new Date(Date.now() + 5 * 60 * 1000)
   return {
     otp,
     expiresAt
