d0b602b2ba55dd1795e948cc9890360fc6ec41e5
omodauda <omodauda.dl@gmail.com>
2022-10-12 01:15:06 +0100

    Reset password validation

--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -98,4 +98,35 @@ const resendVerifyOtp = Joi.object({
     }),
 })
 
-export { signUpValidation, loginValidation, verifyUserValidation, resendVerifyOtp }
+const resetPasswordValidation = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2 })
+    .min(5)
+    .max(100)
+    .required()
+    .messages({
+      'string.empty': 'email cannot be an empty field',
+      'string.email': 'please enter a valid email address',
+      'any.required': 'email is required'
+    }),
+  otp: Joi.string()
+    .length(4)
+    .required()
+    .messages({
+      'string.empty': 'otp cannot be an empty field',
+      'string.length': 'otp should be 4 characters length',
+      'any.required': 'otp is required',
+    }),
+  password: Joi.string()
+    .min(5)
+    .max(20)
+    .required()
+    .messages({
+      'string.empty': 'password cannot be an empty field',
+      'string.min': 'password should be minimum of 5 characters length',
+      'string.max': 'password should not be more than 20 characters length',
+      'any.required': 'password is required',
+    }),
+})
+
+export { signUpValidation, loginValidation, verifyUserValidation, resendVerifyOtp, resetPasswordValidation }
