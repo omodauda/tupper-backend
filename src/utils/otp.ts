722dd2f18cbf3023f7445f460483fcd88bbfb3d4
omodauda <omodauda.dl@gmail.com>
2022-10-19 00:21:15 +0100

    Generate random otp

--- src/utils/otp.ts
+++ src/utils/otp.ts
@@ -1,7 +1,12 @@
-import moment from 'moment';
+import otpGenerator from 'otp-generator';
 
 function generateOtp() {
-  const otp = '1234';
+  const otp = otpGenerator.generate(5, {
+    digits: true,
+    upperCaseAlphabets: false,
+    lowerCaseAlphabets: false,
+    specialChars: false,
+  })
   // const expiresAt = moment().add(6, 'minutes').toString();
   const expiresAt = new Date(Date.now() + 5 * 60 * 1000)
   return {
