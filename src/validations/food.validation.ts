220d4ef44ce9531aa0736e178865db50b1285ce1
omodauda <omodauda.dl@gmail.com>
2022-10-24 00:24:05 +0100

    Add food validation

---
+++ src/validations/food.validation.ts
@@ -0,0 +1,42 @@
+import Joi from "joi";
+
+export const addFoodValidation = Joi.object({
+  storageId: Joi.string()
+    .min(36)
+    .max(36)
+    .required()
+    .messages({
+      'string.empty': 'storage id cannot be an empty field',
+      'string.min': 'storage id should be minimum of 36 characters length',
+      'string.max': 'storage id should not be more than 36 characters length',
+      'any.required': 'storage id is required'
+    }),
+  name: Joi.string()
+    .required()
+    .messages({
+      'string.empty': 'name cannot be an empty field',
+      'any.required': 'name is required'
+    }),
+  quantity: Joi.number()
+    .required()
+    .integer()
+    .positive()
+    .messages({
+      'number.base': "quantity must be a number",
+      'any.required': 'quantity is required',
+      'number.positive': 'quantity should be a positive number',
+      'number.integer': 'quantity should be an integer'
+    }),
+  createdDate: Joi.date()
+    .required()
+    .messages({
+      'date.base': 'created date must be a valid date',
+      'any.required': 'created date is required'
+    }),
+  expiryDate: Joi.date()
+    .required()
+    .messages({
+      'date.base': 'expiry date must be a valid date',
+      'any.required': 'expiry date is required'
+    })
+})
