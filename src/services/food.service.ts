a4b48f6d7f77ed1f04f38b82dae658abc171506d
omodauda <omodauda.dl@gmail.com>
2022-10-24 00:23:28 +0100

    Add food service

--- src/services/food.service.ts
+++ src/services/food.service.ts
@@ -1,6 +1,7 @@
-import HttpException from "utils/handlers/error.handler";
+import HttpException from "../utils/handlers/error.handler";
 import prisma from "../lib/prisma";
 import { Storage } from "@prisma/client";
+import { FoodItem } from "interfaces/food.interface";
 
 export default class FoodService {
   public storage = prisma.storage;
@@ -8,5 +9,23 @@ export default class FoodService {
 
   public async getStorages(): Promise<Storage[]> {
     return await this.storage.findMany();
+  }
+
+  public async addFood(userId: string, foodItemData: FoodItem) {
+    // check if storage exists
+    const storage = await this.storage.findUnique({ where: { id: foodItemData.storageId } })
+    if (!storage) {
+      throw new HttpException(409, 'Invalid storage')
+    }
+    return await this.food.create({
+      data: {
+        userId,
+        storageId: foodItemData.storageId,
+        name: foodItemData.name,
+        quantity: foodItemData.quantity,
+        createdDate: foodItemData.createdDate,
+        expiryDate: foodItemData.expiryDate
+      }
+    })
   }
 }
