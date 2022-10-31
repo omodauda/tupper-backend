5af167fba67a6ec41d7841225922a54ea9be9ce3
omodauda <omodauda.dl@gmail.com>
2022-10-31 16:00:04 +0100

    initialize firebase-admin

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import hpp from 'hpp';
+import admin from 'firebase-admin';
 
 import errorMiddleware from './middlewares/error.middleware';
 import Routes from './interfaces/route.interface';
@@ -12,16 +13,25 @@ class App {
   public app: Application;
   public port: number | string;
   public env: string;
+  private googleKey: string;
+  private serviceAccount: string;
 
   constructor(routes: Routes[]) {
     this.app = express();
     this.port = process.env.PORT || 4000;
     this.env = process.env.NODE_ENV || 'development';
+    this.googleKey = process.env['GOOGLE_APP_CREDENTIALS'] || '';
+    this.serviceAccount = JSON.parse(this.googleKey)
 
     this.initializeMiddleware();
     this.initializeRoutes(routes);
     this.initializeErrorHandling();
-    this.initializeSchedules()
+    this.initializeSchedules();
+
+    admin.initializeApp({
+      credential: admin.credential.cert(this.serviceAccount)
+    })
+
   }
 
   private initializeMiddleware(): void {
