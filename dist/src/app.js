63a130439c124a6e3a8fe423523e402414bbd98c
omodauda <omodauda.dl@gmail.com>
2022-10-31 17:30:14 +0100

    revert service account

--- dist/src/app.js
+++ dist/src/app.js
@@ -17,19 +17,7 @@ class App {
         this.port = process.env.PORT || 4000;
         this.env = process.env.NODE_ENV || 'development';
         this.googleKey = process.env['GOOGLE_APP_CREDENTIALS'] || '';
-        // this.serviceAccount = JSON.parse(this.googleKey)
-        this.serviceAccount = {
-            "type": process.env.type,
-            "project_id": process.env.project_id,
-            "private_key_id": process.env.private_key_id,
-            "private_key": process.env.private_key,
-            "client_email": process.env.client_email,
-            "client_id": process.env.client_id,
-            "auth_uri": process.env.auth_uri,
-            "token_uri": process.env.token_uri,
-            "auth_provider_x509_cert_url": process.env.auth_provider,
-            "client_x509_cert_url": process.env.client_cert_url,
-        };
+        this.serviceAccount = JSON.parse(this.googleKey);
         this.initializeMiddleware();
         this.initializeRoutes(routes);
         this.initializeErrorHandling();
