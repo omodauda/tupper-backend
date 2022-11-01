0eb4a7737554f94ae475d0daa1552716edf6801b
omodauda <omodauda.dl@gmail.com>
2022-11-01 16:51:26 +0100

    refactor: sign token on signup

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -10,12 +10,15 @@ export default class UserController {
   public signUp = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
     try {
       const { name, email, password, zipCode } = req.body;
-      await this.UserService.createUser(name, email, password, zipCode);
+      const user: User = await this.UserService.createUser(name, email, password, zipCode);
+      const { token } = signToken(user);
       return res
         .status(201)
         .json({
           status: 'success',
-          message: 'user successfully registered'
+          message: 'user successfully registered',
+          token,
+          data: user
         })
     } catch (error) {
       next(error);
